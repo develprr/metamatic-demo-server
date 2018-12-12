4763b02a1bfa2f442d424549c1defd1ff35fae70
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-12 15:11:22 +0200

    separated data retrieval functions into separate file

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+const Database = require('./lib/Database.js');
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -14,74 +16,16 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-const getCars = () => [
-  {
-    'id': 1,
-    'model': 'Audi',
-    'speed': 170,
-    'color': 'black',
-    'price': 28500,
-    'marketShare': []
-  },
-  {
-    'id': 2,
-    'model': 'Porsche',
-    'speed': 250,
-    'color': 'white',
-    'price': 61000,
-    'marketShare': []
-  },
-  {
-    'id': 3,
-    'model': 'Tesla',
-    'speed': 280,
-    'color': 'green',
-    'price': 79000,
-    'marketShare': []
-  },
-  {
-    'id': 4,
-    'model': 'Ferrari',
-    'speed': 320,
-    'color': 'red',
-    'price': 344000,
-    'marketShare': []
-  },
-  {
-    'id': 5,
-    'model': 'Lamborghini',
-    'speed': 334,
-    'color': 'yellow',
-    'price': 521000,
-    'marketShare': []
-  },
-  {
-    'id': 6,
-    'model': 'Bugatti',
-    'speed': 420,
-    'color': 'blue',
-    'price': 1591000,
-    'marketShare': []
-  }
-];
-
-const getCarsMap = () => getCars().reduce((map, car) => {
-  map[car.id] = car;
-  return map;
-}, {});
-
-const getCarById = (id) => getCarsMap()[id];
-
 app.get('/', (req, res) => res.render('pages/index'));
 
 app.get('/api/cars', function (req, res) {
   const id = req.params.id;
-  res.json(getCars());
+  res.json(Database.getCars());
 });
 
 app.get('/api/cars/:id', function (req, res) {
   const id = req.params.id;
-  res.json(getCarById(id));
+  res.json(Database.getCarById(id));
 });
 
 module.exports = app;
