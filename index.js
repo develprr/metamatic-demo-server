75c6a316560d9b4f226e2ba2c2a3d1fb107e3014
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-27 10:30:04 +0200

    moved json data into separate files

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-const Database = require('./lib/Database');
+const Database = require("./lib/Database");
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -14,14 +14,14 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/api/cars', function (req, res) {
+app.get("/api/cars", function (req, res) {
   const id = req.params.id;
   res.json(Database.getCars());
 });
 
-app.get('/api/cars/:id', function (req, res) {
+app.get("/api/cars/:id", function (req, res) {
   const id = req.params.id;
   res.json(Database.getCarById(id));
 });
